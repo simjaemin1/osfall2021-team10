98a7dc8cc080438daf93504e8b5d5daf91f72b3d
91bin <nagnebin@gamil.com>
2021-10-08 02:24:53 +0900

    수정함:        kernel/ptree.c

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -8,16 +8,39 @@
 #include <linux/sched.h>    // task_struct (doubly linked list)
 #include <linux/sched/task.h>
 #include <linux/prinfo.h>
+#include <linux/thread_info.h>	// get_current()
+
+/*Getting top process as task_struct*/
+static struct task_struct* get_top_process(void)
+{
+	struct task_struct *tmp;
+	tmp=get_current();
+	printk("current tasks pid: %d name: %s", tmp->pid, tmp->comm);
+    while(tmp->parent!=tmp)
+	{
+		tmp=tmp->parent;
+		printk("current tasks pid: %d name: %s", tmp->pid, tmp->comm);
+	}
+	return tmp;
+}
+
+/*traverse function*/
+void traverse(struct task_struct *top)
+{
+
+}
+
 
 SYSCALL_DEFINE2(ptree, struct prinfo __user *, buf, int __user *, nr)
 {
     /* Initiate variable, list with error check */
-
+	struct task_struct *top;	/*top process's task_struct*/
     int nr_k; /* kernel nr value */
     int cnt = 0; /* counted # of task */
     struct prinfo *buf_k; /* kernel prinfo buffer  */
 
-    if(!buf || !nr) {
+    
+	if(!buf || !nr) {
         printk("ERROR : Invalid argument (buf or nr is null)\n");
         return -EINVAL;
     }
@@ -38,15 +61,16 @@ SYSCALL_DEFINE2(ptree, struct prinfo __user *, buf, int __user *, nr)
         printk("ERROR : struct prinfo buf error\n");
         return -EFAULT;
     }
+	
 
+	top=get_top_process();		//find top process before traverse
     /* Lock tasklist and do something */
     read_lock(&tasklist_lock);
-
     /* End traversal. Unlock and copy to user space */
     read_unlock(&tasklist_lock);
 
     if(cnt < nr_k) nr_k = cnt;
-
+	
     if(copy_to_user(nr, &nr_k, sizeof(int))) {
         printk("ERROR : copy to user space error (nr)\n");
         return -EFAULT;
@@ -56,6 +80,7 @@ SYSCALL_DEFINE2(ptree, struct prinfo __user *, buf, int __user *, nr)
         printk("ERROR : copy to user space error (buf)\n");
         return -EFAULT;
     } 
+	
 
     /* free kmalloc space and return */
 
