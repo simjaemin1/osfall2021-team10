bf926c5ad7ddc821e46cf9d6a33afcf1516d9282
lsh <likey1121@snu.ac.kr>
2021-10-08 01:45:47 +0900

    ptree skeleton2

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -14,10 +14,8 @@ SYSCALL_DEFINE2(ptree, struct prinfo __user *, buf, int __user *, nr)
     /* Initiate variable, list with error check */
 
     int nr_k; /* kernel nr value */
-    int ret; /* return value (counted # of task or nr) */
-    struct prinfo buf_k; /* kernel prinfo buffer value */
-    
-
+    int cnt; /* counted # of task */
+    struct prinfo *buf_k; /* kernel prinfo buffer  */
 
     if(!buf || !nr) {
         printk("ERROR : Invalid argument (buf or nr is null)\n");
@@ -29,21 +27,40 @@ SYSCALL_DEFINE2(ptree, struct prinfo __user *, buf, int __user *, nr)
         return -EFAULT;
     }
 
-    if(nr_k <= 0) {
-        printk("ERROR : nr is not positive");
+    if(nr_k <= 1) {
+        printk("ERROR : nr is less than 1\n");
         return -EINVAL;
     }
 
+    buf_k = (struct prinfo *)kmalloc(nr_k * sizeof(struct prinfo), GFP_KERNEL);
 
+    if(!buf_k) {
+        printk("ERROR : struct prinfo buf error\n");
+        return -EFAULT;
+    }
 
     /* Lock tasklist and do something */
     read_lock(&tasklist_lock);
 
+    /* End traversal. Unlock and copy to user space */
     read_unlock(&tasklist_lock);
 
+    if(cnt < nr_k) nr_k = cnt;
 
-    /* return  */
-    ret = (ret > nr) ? nr : ret;
-    return ret;
+    if(copy_to_user(nr, &nr_k, sizeof(int))) {
+        printk("ERROR : copy to user space error (nr)\n");
+        return -EFAULT;
+    }
+
+    if(copy_to_user(buf, &buf_k, sizeof(struct prinfo)*nr_k)) {
+        printk("ERROR : copy to user space error (buf)\n");
+        return -EFAULT;
+    } 
+
+    /* free kmalloc space and return */
+
+    kfree(buf_k);
+
+    return nr_k;
 }
 
