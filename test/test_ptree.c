ef28a4374109af1625e7b0665a0431078c3ca915
lsh <likey1121@snu.ac.kr>
2021-10-08 05:28:20 +0900

    test example

--- test/test_ptree.c
+++ test/test_ptree.c
@@ -2,9 +2,21 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
+#include <stdlib.h>
+#include <linux/prinfo.h>
 
 int main(int argc, char *argv[])
 {
-	syscall(398);
+    int *nr = malloc(sizeof(int));
+    *nr = 10;
+    struct prinfo *prlist;
+    prlist = (struct prinfo *)malloc(sizeof(struct prinfo) * (*nr));
+	syscall(398, prlist, nr);
+
+    // printf or something
+
+    free(prlist);
+    free(nr);
+    return 0;
 }
 	
