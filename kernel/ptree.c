9394e607cf278b851c4873f77bb614a96c165152
lsh <likey1121@snu.ac.kr>
2021-10-07 23:13:28 +0900

    ptree skeleton

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -11,7 +11,39 @@
 
 SYSCALL_DEFINE2(ptree, struct prinfo __user *, buf, int __user *, nr)
 {
-    printk("syscall ptree working test\n");
-    return 0;
+    /* Initiate variable, list with error check */
+
+    int nr_k; /* kernel nr value */
+    int ret; /* return value (counted # of task or nr) */
+    struct prinfo buf_k; /* kernel prinfo buffer value */
+    
+
+
+    if(!buf || !nr) {
+        printk("ERROR : Invalid argument (buf or nr is null)\n");
+        return -EINVAL;
+    }
+
+    if(copy_from_user(&nr_k, nr, sizeof(int))) {
+        printk("ERROR : nr reading error\n");
+        return -EFAULT;
+    }
+
+    if(nr_k <= 0) {
+        printk("ERROR : nr is not positive");
+        return -EINVAL;
+    }
+
+
+
+    /* Lock tasklist and do something */
+    read_lock(&tasklist_lock);
+
+    read_unlock(&tasklist_lock);
+
+
+    /* return  */
+    ret = (ret > nr) ? nr : ret;
+    return ret;
 }
 
