022dec4f4dbdb81a59e0e8a2ecda6e08adf82611
91bin <nagnebin@gamil.com>
2021-10-08 09:59:26 +0900

    수정함:        test/test_ptree.c

--- test/test_ptree.c
+++ test/test_ptree.c
@@ -6,18 +6,42 @@
 #include <linux/prinfo.h>
 #include <stdio.h>
 
+
+void print_process_tree(struct prinfo *plist, int size)
+{
+	struct prinfo p;
+	int *pid_stack=(int*) malloc(sizeof(int)*size);
+	int ind;
+	p = plist[0];
+	printf("%s,%d,%lld,%d,%d,%d,%lld\n", p.comm, p.pid, p.state, p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
+	pid_stack[0] =p.pid;
+	for (int i=1; i<size; i++)
+	{
+		ind=0;
+		p=plist[i];
+		do{printf("   ");}while(pid_stack[ind++]!=p.parent_pid);
+		printf("%s, %d, %lld, %d, %d, %d, %lld\n", p.comm, p.pid, p.state, p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
+		pid_stack[ind]=p.pid;
+	}
+
+}
+
+
 int main(int argc, char *argv[])
 {
-	int *nr=(int*)malloc(sizeof(int));
-	*nr = 200;
-	struct prinfo *plist;
-	struct prinfo p;
-	plist = (struct prinfo*)malloc(sizeof(struct prinfo)*(*nr));
-	int nr_k = syscall(398, plist, nr);
-	for (int i=0; i<nr_k; i++)
+	if(argc!=2)
 	{
-		p=plist[i];
-		printf("%s,%d,%lld,%d,%d,%d,%lld\n", p.comm, p.pid, p.state, p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
+		printf("usage: /test (number of entries)");
+		return -1;
 	}
-}
+	int *nr=(int*)malloc(sizeof(int));
+	*nr = atoi(argv[1]);
+	struct prinfo *plist = (struct prinfo*)malloc(sizeof(struct prinfo)*(*nr));
 	
+	int nr_k = syscall(398, plist, nr);
+	print_process_tree(plist, nr_k);
+	free(nr);
+	free(plist);
+}
+
+
