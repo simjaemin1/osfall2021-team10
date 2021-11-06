8303c23e537dda438e9fe1941761954a249bd2f3
91bin <nagnebin@gamil.com>
2021-11-06 10:02:09 +0900

    수정함:        kernel/sched/sched.h

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -136,6 +136,10 @@ static inline int rt_policy(int policy)
 	return policy == SCHED_FIFO || policy == SCHED_RR;
 }
 
+static inline int wrr_policy(int policy)
+{
+	return policy == SCEHD_WRR;
+}
 static inline int dl_policy(int policy)
 {
 	return policy == SCHED_DEADLINE;
@@ -143,7 +147,7 @@ static inline int dl_policy(int policy)
 static inline bool valid_policy(int policy)
 {
 	return idle_policy(policy) || fair_policy(policy) ||
-		rt_policy(policy) || dl_policy(policy);
+		rt_policy(policy) || dl_policy(policy) || wrr_policy(policy);
 }
 
 static inline int task_has_rt_policy(struct task_struct *p)
