384050ab3b26e02e0c3d65d916fdee315114b0ae
91bin <nagnebin@gamil.com>
2021-11-06 18:15:28 +0900

    수정함:        run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ sudo ./build-rpi3-arm64.sh;
 sudo ./scripts/mkbootimg_rpi3.sh;
 sudo mv boot.img modules.img ../tizen-image;
 sudo mount ../tizen-image/rootfs.img ../mnt_dir;
-cd ./test; arm-linux-gnueabi-gcc -I../include ./test.c -o test; cd ../
+cd ./test; arm-linux-gnueabi-gcc -I../include ./test.c -o test; cd ../;
+cd ./test; arm-linux-gnueabi-gcc -I../include ./test_schedule.c -o test_schedule; cd ../;
 sudo cp ./test/test ../mnt_dir/root/;
+sudo cp ./test/test_schedule ../mnt_dir/root/;
 sudo ./qemu.sh;
