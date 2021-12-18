b4bf41d242fb16507c3fe7c89f444ed33a342310
91bin <nagnebin@gamil.com>
2021-12-18 19:21:29 +0900

    수정함:        test/file_loc.c 	수정함:        test/gpsupdate.c

--- test/file_loc.c
+++ test/file_loc.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/syscall.h>
+#include <linux/gps.h>
 
+#define GET_GPS_LOCATION 399
 
 int main(int argc, char *argv[])
 {
+	struct gps_location loc;
 	if(argc !=2)
 	{
 		printf("Usage: /test (filename)\n");
 		return -1;
 	}
 	int error;
-	error = syscall(399, argv[1], &loc);
-
+	error = syscall(GET_GPS_LOCATION, argv[1], &loc);
+	if(error)
+		printf("error code: %d\n", error);
+	else{
+		printf("----------File info---------\n");
+		printf("GPS coordinate:	(latitude,longitude)/accuracy = (%d.%d,	%d.%d)/%d\n", loc.lat_integer, loc.lat_fractional, loc.lng_integer, loc.lat_fractional, loc.accuracy);
+		printf("Google map linki:	https://www.google.co.kr/maps/place/@%d.%d,%d.%d,%dz/\n", loc.lat_integer, loc.lat_fractional, loc.lng_integer, loc.lat_fractional, loc.accuracy);
+	}
+	return 0;
 }
--- test/gpsupdate.c
+++ test/gpsupdate.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <sys.syscall.h>
+#include <linux/gps.h>
 
+#define SET_GPS_LOCATION 398
+
+
+int main()
+{
+	struct gps_location loc={1,1,1,1,1};//change
+
+	change_current_location(&loc, 1, 2, 1);
+	set_location(&loc);	
+	return 0;
+}
+
+void change_current_location(struct gps_location *loc, int lat_change, int lng_change, int newaccuracy)
+{	
+	//TODO
+}
+
+void set_location(struct gps_location *loc)
+{
+	int error;
+	error = syscall(SET_GPS_LOCATION, loc);
+	if(error){
+		printf("error code: %d\n", error)
+		return -1;
+	}
+}
